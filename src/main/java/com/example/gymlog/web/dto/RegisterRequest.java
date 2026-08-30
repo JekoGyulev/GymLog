18fe38dde2be97e5532aa6a0daa7f3385ec4218d
JekoGyulev <165460153+JekoGyulev@users.noreply.github.com>
2026-08-30 19:13:19 +0300

    -> modify RegisterRequest DTO -> add register HTML page and CSS styling for it -> add css class alert-warning for styling of errors

--- src/main/java/com/example/gymlog/web/dto/RegisterRequest.java
+++ src/main/java/com/example/gymlog/web/dto/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.example.gymlog.web.dto;
 
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -13,19 +14,16 @@ import lombok.*;
 @AllArgsConstructor
 public class RegisterRequest {
 
-
-    @Size(min = 2, message = "Your first name must be at least 2 characters long")
-    private String firstName;
-    @Size(min = 2, message = "Your last name must be at least 2 characters long")
-    private String lastName;
+    @NotBlank(message = "Your full name must be filled in")
+    private String fullName;
     @Size(min = 5, message = "Your username must be at least 5 characters long")
     private String username;
     @Size(min = 5, message = "Your password must be at least 5 characters long")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$", message = "Your password is too weak")
     private String password;
-    @Size(min = 5, message = "Your confirm password must be at least 5 characters long")
+    @NotBlank(message = "The password confirmation field must be filled in")
     private String confirmPassword;
-    @Email(message = "Enter a valid email")
+    @Email(message = "Your email is invalid")
     private String email;
 
 
